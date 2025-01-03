f3735bce28e2ed467037f2fe3061444f073b53d3
Maria Fernanda Rizzo <mahcosta54@gmail.com>
2025-01-03 17:37:32 -0300

    add: implement core styles and components for mobile app

---
+++ mobile/src/components/welcome/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import {colors, fontFamily} from "@/styles/theme";
+
+export const s = StyleSheet.create({
+    logo: {
+        width: 48,
+        height: 48,
+        marginBottom: 28,
+        marginTop: 24,
+    },
+    title: {
+        fontFamily: fontFamily.bold,
+        fontSize: 24,
+        color: colors.gray[600],
+
+    },
+    subtitle: {
+        fontFamily: fontFamily.regular,
+        fontSize: 16,
+        color: colors.gray[500],
+        marginTop: 12,
+    },
+})
